f25e52042d8ce1499aecd9fcb870543694457929
Мария Проскурина <patrick.delroy.yohoho@gmail.com>
2020-06-01 14:26:08 +0300

    сделала 5-1

--- hw5/hw5-1.py
+++ hw5/hw5-1.py
@@ -9,28 +9,30 @@ companies = []
 
 
 def input_company_data():
-    company.name = input('Введите название компании ')
-    company.q1 = input('Введите прибыль за 1 квартал ')
-    company.q2 = input('Введите прибыль за 2 квартал ')
-    company.q3 = input('Введите прибыль за 3 квартал ')
-    company.q4 = input('Введите прибыль за 4 квартал ')
-    company.common = (company.q1 + company.q2 + company.q3 + company.q4) / 4
-    companies.append(company)
+    name = input('Введите название компании ')
+    q1 = int(input('Введите прибыль за 1 квартал '))
+    q2 = int(input('Введите прибыль за 2 квартал '))
+    q3 = int(input('Введите прибыль за 3 квартал '))
+    q4 = int(input('Введите прибыль за 4 квартал '))
+    common = (q1 + q2 + q3 + q4) / 4
+    return company(name, q1, q2, q3, q4, common)
 
 
 number_of_companies = int(input('Введите количество компаний '))
 
-for i in range(number_of_companies):
-    input_company_data()
+for i in range(0, number_of_companies):
+    companies.append(input_company_data())
     i += 1
 
 max_common = [companies[0].common, 0]
-for i in range(number_of_companies):
-    if companies[i].common > max_common:
+for i in range(0, number_of_companies):
+    if companies[i].common > max_common[0]:
         max_common = [companies[i].common, i]
+    i += 1
 
-for i in range(number_of_companies):
+for i in range(0, number_of_companies):
     print(companies[i].name, ' Средняя годовая выручка = ', companies[i].common)
+    i += 1
 
 print('Компания с максимальной выручкой - ', companies[max_common[1]].name)
 
