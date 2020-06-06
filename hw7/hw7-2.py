c697900f99ae68853ee9a4cb60d08ab70eb7246c
Мария Проскурина <patrick.delroy.yohoho@gmail.com>
2020-06-06 21:15:39 +0300

    hw7-2 есть слияние массива

--- hw7/hw7-2.py
+++ hw7/hw7-2.py
@@ -2,6 +2,7 @@
 # заданный случайными числами на промежутке [0; 50). Выведите на экран исходный и отсортированный массивы.
 
 import random
+from collections import deque
 
 SIZE = 10
 MIN_ITEM = 0.0
@@ -10,14 +11,35 @@ MAX_ITEM = 49.0
 array = [random.uniform(MIN_ITEM, MAX_ITEM) for _ in range(SIZE)]
 print(array)
 
-if SIZE % 2 == 0:
-    left = array[0: int(SIZE / 2)]
-    right = array[int(SIZE / 2): SIZE]
-else:
-    left = array[0: int(SIZE / 2 - 0.5)]
-    right = array[int(SIZE / 2 + 0.5): SIZE]
-print(left)
-print(right)
 
-def sort_m(a: array):
-    
+def merge_array(l: deque, r: deque):
+    res = deque()
+    while l and r:
+        a = l.pop()
+        b = r.pop()
+        if a > b:
+            res.append(b)
+            res.append(a)
+        else:
+            res.append(a)
+            res.append(b)
+    return res
+
+
+def split(a: array):
+    if len(a) % 2 == 0:
+        l = deque(array[0: int(len(a) / 2)])
+        r = deque(array[int(len(a) / 2): len(a)])
+    else:
+        l = deque(array[0: int(len(a) / 2 - 0.5)])
+        r = deque(array[int(len(a) / 2 + 0.5): len(a)])
+    return l, r
+
+
+def sort_array(a: array):
+    l,r = split(a)
+
+
+print(merge_array(left, right))
+
+
