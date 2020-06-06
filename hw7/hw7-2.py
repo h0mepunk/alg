4bc43ddc784534b56dd23bf844ef587e694233f4
Мария Проскурина <patrick.delroy.yohoho@gmail.com>
2020-06-07 00:47:24 +0300

    hw7-2 надо поправить флаг

--- hw7/hw7-2.py
+++ hw7/hw7-2.py
@@ -10,17 +10,19 @@ MAX_ITEM = 49.0
 array = [random.randint(MIN_ITEM, MAX_ITEM) for _ in range(SIZE)]
 print(array)
 buf = []
+res = []
 
 
 def merge_array(l: array, r: array):
-    res = []
     while l and r:
         if l[0] > r[0]:
             res.append(r.pop(0))
             res.append(l.pop(0))
+            return res
         else:
             res.append(l.pop(0))
             res.append(r.pop(0))
+            return res
     if l:
         res.append(l[0])
     if r:
@@ -28,52 +30,37 @@ def merge_array(l: array, r: array):
     return res
 
 
-def write_buf_to_a(first: int, num: int):
-    for i in range(first, num):
-        array[i] = buf.pop(i - num)
+def write_buf_to_a(first: int, num: int, buf: array, a: array):
+    iter = 0
+    for i in range(0, num):
+        a[i + first] = buf.pop(i - iter)
+        i += 1
+        iter += 1
 
 
 def sort_array(a: array, fl: bool, chunk_size: int, first_el):
     if fl == False: # сплиттим
         if chunk_size > 1:
             if chunk_size % 2 == 0:
-                chunk_size /= 2
                 sort_array(a, False, chunk_size / 2 - 1, first_el)
                 sort_array(a, False, chunk_size / 2, chunk_size / 2)
             else:
                 sort_array(a, False, chunk_size / 2 - 0.5, first_el)
                 sort_array(a, False, chunk_size / 2 + 0.5, chunk_size / 2 - 0.5)
         else:
-            if chunk_size % 2 == 0:
-                buf = merge_array(a[first_el], a[first_el + 1])
-                write_buf_to_a(first_el, 2)
-                first_el += 2
-
-            else:
-                buf = merge_array(a[first_el: first_el + 1], a[first_el + 3])
-                write_buf_to_a(first_el, 3)
-                first_el += 3
+            fl = False
+            buf = merge_array([a[first_el]], [a[first_el + 1]])
+            write_buf_to_a(first_el, 2, buf, array)
+            first_el += 2
     else:
         if chunk_size == len(a):
             return a
         else:
             buf = merge_array(a[first_el, chunk_size / 2], a[chunk_size / 2, chunk_size])
-            write_buf_to_a(first_el, chunk_size)
+            write_buf_to_a(first_el, chunk_size, buf, array)
             chunk_size *= 2
             first_el += chunk_size
+            sort_array(a, True, chunk_size, first_el)
 
 
-
-
-
-
-
-
-
-
-
-
-
-# print(merge_array([1, 4], [7]))
-
-print(sort_array(array))
+print(sort_array(array, False, len(array), 0))
