7317be4c24225e41096aca8552b38e28dcb5379a
Мария Проскурина <patrick.delroy.yohoho@gmail.com>
2020-06-07 00:10:30 +0300

    hw7-2 черновик слияния соединением

--- hw7/hw7-2.py
+++ hw7/hw7-2.py
@@ -2,44 +2,78 @@
 # заданный случайными числами на промежутке [0; 50). Выведите на экран исходный и отсортированный массивы.
 
 import random
-from collections import deque
 
-SIZE = 10
+SIZE = 9
 MIN_ITEM = 0.0
 MAX_ITEM = 49.0
 
-array = [random.uniform(MIN_ITEM, MAX_ITEM) for _ in range(SIZE)]
+array = [random.randint(MIN_ITEM, MAX_ITEM) for _ in range(SIZE)]
 print(array)
+buf = []
 
 
-def merge_array(l: deque, r: deque):
-    res = deque()
+def merge_array(l: array, r: array):
+    res = []
     while l and r:
-        a = l.pop()
-        b = r.pop()
-        if a > b:
-            res.append(b)
-            res.append(a)
+        if l[0] > r[0]:
+            res.append(r.pop(0))
+            res.append(l.pop(0))
         else:
-            res.append(a)
-            res.append(b)
+            res.append(l.pop(0))
+            res.append(r.pop(0))
+    if l:
+        res.append(l[0])
+    if r:
+        res.append(r[0])
     return res
 
 
-def split(a: array):
-    if len(a) % 2 == 0:
-        l = deque(array[0: int(len(a) / 2)])
-        r = deque(array[int(len(a) / 2): len(a)])
+def write_buf_to_a(first: int, num: int):
+    for i in range(first, num):
+        array[i] = buf.pop(i - num)
+
+
+def sort_array(a: array, fl: bool, chunk_size: int, first_el):
+    if fl == False: # сплиттим
+        if chunk_size > 1:
+            if chunk_size % 2 == 0:
+                chunk_size /= 2
+                sort_array(a, False, chunk_size / 2 - 1, first_el)
+                sort_array(a, False, chunk_size / 2, chunk_size / 2)
+            else:
+                sort_array(a, False, chunk_size / 2 - 0.5, first_el)
+                sort_array(a, False, chunk_size / 2 + 0.5, chunk_size / 2 - 0.5)
+        else:
+            if chunk_size % 2 == 0:
+                buf = merge_array(a[first_el], a[first_el + 1])
+                write_buf_to_a(first_el, 2)
+                first_el += 2
+
+            else:
+                buf = merge_array(a[first_el: first_el + 1], a[first_el + 3])
+                write_buf_to_a(first_el, 3)
+                first_el += 3
     else:
-        l = deque(array[0: int(len(a) / 2 - 0.5)])
-        r = deque(array[int(len(a) / 2 + 0.5): len(a)])
-    return l, r
+        if chunk_size == len(a):
+            return a
+        else:
+            buf = merge_array(a[first_el, chunk_size / 2], a[chunk_size / 2, chunk_size])
+            write_buf_to_a(first_el, chunk_size)
+            chunk_size *= 2
+            first_el += chunk_size
 
 
-def sort_array(a: array):
-    l,r = split(a)
 
 
-print(merge_array(left, right))
 
 
+
+
+
+
+
+
+
+# print(merge_array([1, 4], [7]))
+
+print(sort_array(array))
