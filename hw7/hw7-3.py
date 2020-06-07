e61e880e20c6773360a8dcbd27a49d0da92c5111
Мария Проскурина <patrick.delroy.yohoho@gmail.com>
2020-06-07 21:08:38 +0300

    hw7-3 сделала но очень неоптимально

--- hw7/hw7-3.py
+++ hw7/hw7-3.py
@@ -8,9 +8,44 @@
 
 import random
 
-SIZE = 10
-MIN_ITEM = 0.0
+SIZE = 9
+MIN_ITEM = -50.0
 MAX_ITEM = 49.0
 
-array = [random.uniform(MIN_ITEM, MAX_ITEM) for _ in range(SIZE)]
+array = [random.randint(MIN_ITEM, MAX_ITEM) for _ in range(SIZE)]
 print(array)
+
+
+def med_search(a: array):
+    flag_med_found = False
+    if all(el < a[0] for el in set(a[1: SIZE])) or all(el < a[0] for el in set(a[1: SIZE])):
+        flag_med_found = True
+        return a[0]
+    if all(el > a[0] for el in set(a[0: SIZE -1])) or all(el < a[0] for el in set(a[0: SIZE - 1])):
+        flag_med_found = True
+        return a[SIZE]
+
+    for i in range(1, SIZE - 1):
+        right = set(a[i + 1: SIZE])
+        left = set(a[0: i - 1])
+        if all(el > a[i] for el in left):
+            if all(el < a[i] for el in right):
+                flag_med_found = True
+                return a[i]
+            else:
+                i += 1
+        else:
+            if all(el < a[i] for el in left):
+                if all(el > a[i] for el in right):
+                    flag_med_found = True
+                    return a[i]
+                else:
+                    i += 1
+            else:
+                i += 1
+    if not flag_med_found:
+        return "Медиана не найдена"
+
+
+print(med_search(array))
+
